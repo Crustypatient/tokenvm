d4a4291ca620937de2ee60e24075feeb9146644e
Crustypatient <crustypatient@gmail.com>
2024-07-14 17:24:32 +0000

    init

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "CRUSTYPATIENT-HRP"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = CRYPAT""
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "CP"
 )
 
 var ID ids.ID
